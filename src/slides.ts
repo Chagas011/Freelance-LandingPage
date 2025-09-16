1285f0015081e7728160452a5c6f625117f28de3
Chagas011 <washington.chagas.9@hotmail.com>
2025-09-15 23:01:36 -0300

    image bug

--- src/slides.ts
+++ src/slides.ts
@@ -2,9 +2,9 @@ export const slidesCarousel = [
   "/carrousel2.jpg",
   "/carrousel5.jpg",
   "/carrousel6.jpg",
-  "/carrousel7.jpg",
-  "/carrousel8.jpg",
-  "/carrousel9.jpg",
+  "/carrousel7.JPG",
+  "/carrousel8.JPG",
+  "/carrousel9.JPG",
 ];
 
 export const services = ["/unha.jpg", "/lixa.jpg", "/pe.jpg", "/agenda.jpg"];
